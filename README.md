a718ed74c05e13e4e8ec33282ff7fc7575f5ca20
Seung Tack Jo <jstack87215@gmail.com>
2020-05-19 23:25:24 +0900

    use spinkit

---
+++ README.md
@@ -0,0 +1,11 @@
+# flutter_weather_app
+
+A new Flutter application.
+
+## 20200519
+
+Use package:flutter_spinkit 다양한 로딩화면 자세한 내용 ref
+
+### ref
+
+- https://pub.dev/packages/flutter_spinkit#-readme-tab-
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather App',
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
